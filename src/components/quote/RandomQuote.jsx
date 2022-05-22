b3cdb95493e9fb1c2cc58631793cf93a3be5b060
bhushannaikaroskar <bhushannaikaroskar@gmail.com>
2022-05-23 01:29:34 +0530

    fix: solve edge case for extreme number

--- src/components/quote/RandomQuote.jsx
+++ src/components/quote/RandomQuote.jsx
@@ -4,7 +4,7 @@ import "./randomQuote.css"
 
 export default function RandomQuote() {
 
-  const [quoteObj,setQuoteObj] = useState(quotes[Math.floor(Math.random()*quotes.length)])
+  const [quoteObj,setQuoteObj] = useState(quotes[Math.floor(Math.random()*(quotes.length-1))])
 
   return (
     <div className='quote-container'>
